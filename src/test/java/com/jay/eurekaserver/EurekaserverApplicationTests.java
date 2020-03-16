f99e6b02568ed98334c9c951e13a04834169c589
liushuai@ideal-link.com <shuai515>
2020-03-16 17:50:48 +0800

    Initial commit

---
+++ src/test/java/com/jay/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jay.eurekaserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

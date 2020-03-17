dc366ec036b5a40e3ebd5e9c20e3f69a080580f4
liushuai@ideal-link.com <shuai515>
2020-03-17 21:06:15 +0800

    1

--- src/main/java/com/jay/eurekaserver/EurekaserverApplication.java
+++ src/main/java/com/jay/eurekaserver/EurekaserverApplication.java
@@ -2,10 +2,13 @@ package com.jay.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//配置为注册中心
 @EnableEurekaServer
 @SpringBootApplication
+//配置 配置中心
+@EnableConfigServer
 public class EurekaserverApplication {
 
     public static void main(String[] args) { SpringApplication.run(EurekaserverApplication.class, args);
